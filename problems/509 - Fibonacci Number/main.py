ba0840e4c7f52331dfdc7b62076db24e92c087a1
mirzamukkarambaig <mirza.mukkaram.baig@outlook.com>
2025-01-06 17:17:37 +0500

    Q. 509, 83

---
+++ problems/509 - Fibonacci Number/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+
+        a = 0
+        b = 1
+
+        for _ in range(n - 1):
+            a, b = b, a+b
+    
+        return b
+        
